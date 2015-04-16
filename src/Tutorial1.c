6680bca735f536429a908da4a29457085b074ebd
Jimmy Ondecko <jondecko@gmail.com>
2015-04-15 22:59:04 -0400

    updates for basic watchface

--- src/Tutorial1.c
+++ src/Tutorial1.c
@@ -1,1 +1,31 @@
 #include <pebble.h>
+
+static Window *s_main_window;
+
+static void main_window_load(Window *window) {
+}
+
+static void main_window_unload(Window *window) {
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
