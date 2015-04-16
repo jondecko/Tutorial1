f3a59e68622e9f3d52a0743e2f6915811644edba
Jimmy Ondecko <jondecko@gmail.com>
2015-04-16 00:52:18 -0400

    display time

--- src/Tutorial1.c
+++ src/Tutorial1.c
@@ -23,6 +23,31 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
 }
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H,%M", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I,%M", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  //update_time();
+}
+
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
@@ -33,8 +58,14 @@ static void init() {
     .unload = main_window_unload
   });
 
+  // Register with TickTimeService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  // Make sure the time is displayed from the start
+  update_time();
 }
 
 static void deinit() {
